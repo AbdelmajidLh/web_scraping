3960990f1416fae3d7085b6af31727a45c139e64
Abdelmajid EL HOU <abdelmajid.elhou@gmail.com>
2022-10-08 19:48:03 +0200

    unable proxy

--- simple_amazon_scraper/main.py
+++ simple_amazon_scraper/main.py
@@ -8,14 +8,25 @@ headers = {
     'Accept-Language': 'en-US, en;q=0.5'
 }
 
+# inserer le nom du produit
+search_query = input('Entrez le nom du produit :')
+
+
 search_query = 'winter jacket'.replace(' ', '+')
 base_url = 'https://www.amazon.com/s?k={0}'.format(search_query)
 print(base_url)
 
+# disable your proxies in your requests
+proxies = {
+  "http": None,
+  "https": None,
+}
+
+
 items = []
 for i in range(1, 5):
     print('Processing {0}...'.format(base_url + '&page={0}'.format(i)))
-    response = requests.get(base_url + '&page={0}'.format(i), headers=headers)
+    response = requests.get(base_url + '&page={0}'.format(i), headers=headers, proxies=proxies)
     soup = BeautifulSoup(response.content, 'html.parser')
     
     results = soup.find_all('div', {'class': 's-result-item', 'data-component-type': 's-search-result'})
